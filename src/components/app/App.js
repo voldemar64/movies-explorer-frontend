cbadfbca60bf0c2f9dcd204c6704a7a833ddd77f
Vladimir Kostyuk <vodemar22864@gmail.com>
2022-10-05 00:02:06 +0300

    a

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -176,7 +176,7 @@ function App() {
   }
 
   function handleLikeMovie(movie) {
-    const liked = localSavedMovies.some((i) => movie.movieId === i.movieId);
+    const liked = localSavedMovies.some((i) => movie.id === i.movieId);
 
     if (!liked) {
       mainApi.saveMovie(movie)
