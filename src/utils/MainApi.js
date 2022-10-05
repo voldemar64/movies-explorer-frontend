bce3942e741eb8583579a93d00701c22ba71360f
Vladimir Kostyuk <vodemar22864@gmail.com>
2022-10-05 16:26:36 +0300

    fixed: movie info

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -65,7 +65,7 @@ class MainApi {
         description: movie.description,
         image: `https://api.nomoreparties.co${movie.image.url}`,
         trailerLink: movie.trailerLink,
-        thumbnail: `https://api.nomoreparties.co${movie.thumbnail}`,
+        thumbnail: `https://api.nomoreparties.co${movie.image.formats.thumbnail.url}`,
         movieId: movie.id,
         nameRU: movie.nameRU,
         nameEN: movie.nameEN,
