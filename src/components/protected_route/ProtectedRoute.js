909d32fe2f19c34fba95a4829d6c57b994734996
Vladimir Kostyuk <vodemar22864@gmail.com>
2022-10-11 10:45:32 +0300

    redirection

--- src/components/protected_route/ProtectedRoute.js
+++ src/components/protected_route/ProtectedRoute.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
+import Loader from "../loader_spin/Loader";
 
 function ProtectedRoute({ component: Component, ...props }) {
 
   return (
-    props.loggedIn ? <Component {...props}/> : <Redirect to="/"/>
+    props.loggedIn ? <Component {...props}/> : <Loader/>
   )
 }
 
